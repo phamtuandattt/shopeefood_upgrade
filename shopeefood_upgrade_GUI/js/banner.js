0c8f60c27e8c048f11c5fa22344aa689f8d42ee5
DESKTOP-KD2BPDJ\TUAN DAT <phamtuandat16072001@gmail.com>
2025-07-03 15:17:06 +0700

    Add loading-box for banner

--- shopeefood_upgrade_GUI/js/banner.js
+++ shopeefood_upgrade_GUI/js/banner.js
@@ -1,3 +1,6 @@
+// Simulate data loading (e.g., 2 seconds)
+
+
 document.addEventListener("DOMContentLoaded", function () {
     /* ----- SHOW SEARCH LIST -----*/
     var searchInput = document.querySelector("#txtSearchHome");
@@ -12,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
         clearTimeout(typingTimer); // Clear previous timer
         resultDiv.style.display = "none";
         loaderDiv.style.display = "grid"; // Hide div while typing
-        
+
         if (searchInput.value !== "") {
             typingTimer = setTimeout(function () {
                 resultDiv.style.display = "block"; // Show after delay
@@ -31,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function scrolling() {
         var fixedDiv = document.getElementById("banner-left");
         var scrollableDiv = document.getElementById("banner-right").offsetHeight;
-    
+
         if (document.documentElement.scrollTop >= scrollableDiv - 770) {
             fixedDiv.style.position = "absolute";
             fixedDiv.style.top = scrollableDiv - 700 + "px";
@@ -43,4 +46,34 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+
+window.addEventListener('DOMContentLoaded', function () {
+    const loadingBox = document.querySelectorAll('.main-banner-right-home .list-restaurant .now-loading-restaurant');
+    const itemResList = document.querySelectorAll('.main-banner-right-home .list-restaurant .item-restaurant');
+
+    const loadingBoxDisList = document.querySelector('.main-banner-right-home .list-restaurant-district .now-loading-restaurant');
+    const itemDisList = this.document.querySelectorAll('.main-banner-right-home .list-restaurant-district .item-restaurant-district');
+
+    // Show loading box
+    // loadingBox.style.display = 'block';
+    loadingBox.forEach(item => item.style.display = 'block');
+    loadingBoxDisList.style.display = 'block';
+
+    // Hide all .item-res initially (in case CSS is not applied)
+
+    itemResList.forEach(item => item.style.display = 'none');
+    itemDisList.forEach(item => item.style.display = 'none');
+
+    // Simulate loading
+    setTimeout(() => {
+        // Hide loading box
+        // loadingBox.style.display = 'none';
+        loadingBox.forEach(item => item.style.display = 'none');
+        loadingBoxDisList.style.display = 'none';
+
+        // Show all item-res elements
+        itemResList.forEach(item => item.style.display = 'block');
+        itemDisList.forEach(item => item.style.display = 'block');
+    }, 2000);
+});
 
