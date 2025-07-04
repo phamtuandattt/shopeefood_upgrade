27cec73f120485e76d7886c4e4d6bdc01179172c
DESKTOP-KD2BPDJ\TUAN DAT <phamtuandat16072001@gmail.com>
2025-07-04 07:51:17 +0700

    Add and config ASP.NET Core Web MVC project

---
+++ ShopeeFood_WebApp/ShopeeFood_WebApp/Program.GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.AspNetCore.Mvc;
+//global using AutoMapper;
+//global using Newtonsoft.Json;
+//global using System.Net;
+//global using ShopeeFood_WebAPI.DAL.Models;
+//global using ShopeeFood_WebAPI.Infrastructure.Common;
---
+++ ShopeeFood_WebApp/ShopeeFood_WebApp/Program.RegisterDependentServices.cs
@@ -0,0 +1,14 @@
+﻿namespace ShopeeFood_WebApp
+{
+    public static class RegisterDependentServices
+    {
+        public static WebApplicationBuilder RegisterServices(this WebApplicationBuilder builder)
+        {
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            return builder;
+        }
+    }
+}
