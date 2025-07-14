05dbc1141f97aa04be9f4906030364ed2a807ce8
DESKTOP-KD2BPDJ\TUAN DAT <phamtuandat16072001@gmail.com>
2025-07-14 16:05:10 +0700

    Create FooterView Component

---
+++ ShopeeFood_WebApp/ShopeeFood_WebApp/Controllers/Components/FooterViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using ShopeeFood.BLL.ApplicationServices;
+using ShopeeFood.BLL.ServicesContract.BusinessServicesContract;
+using ShopeeFood_WebApp.Models.Cities;
+
+namespace ShopeeFood_WebApp.Controllers.Components
+{
+    [ViewComponent]
+    public class FooterViewComponent : ViewComponent
+    {
+        const string FooterFile = "_Footer";
+
+        private IHttpContextAccessor httpContextAccessor;
+
+        public FooterViewComponent(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            // Handle logic to fetch data
+
+            return View(FooterFile);
+        }
+    }
+}
